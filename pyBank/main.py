ec32fbb7180d99fb66ada7d93a7aa023213fb207
Rock2638 <162598415+Rock2638@users.noreply.github.com>
2024-04-02 17:06:36 +0800

    Financial analysis pyBank comnpleted

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,67 +1,85 @@
-# First import the os module to create file paths across operating sytems
+#First import the os module to create file paths across operating sytems
 import os
 
 #import the module for reading the csv files
 import csv
 
+#set path for file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 print(csvpath)
 
-# Initialize variables to track the rows and the net total amount
+#initialize variables to track the rows and the net total amount
 row_count = 0
 net_total= 0
 
-# Initialize variables to track the chances and its date
+#initialize variables to track the changes and the dates
 greatest_increase = None
 greatest_decrease = None
 greatest_increase_date = ''
-change = 0
+greatest_decrease_date = ''
+change = None
 
 
-# Initialize variables to store the previous month's profit
+#initialize variables to store the previous month's profit
 prev_month_profit = None
 
-
+#open the csv
 with open(csvpath) as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
+    #CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',') 
 
     #skip the header row
     next (csvreader)
     
+    #initialise variable to store the list of profit and loss
     profit_losslist = []
 
+    #for each row in the csv file after the header row
     for row in csvreader:
+        #count the rows to obtain the total number of months
         row_count = row_count+1
+
+        #do a running total of the profits/losses in column 1
         net_total = net_total + int(row[1])
+
+        #add profits/losses to a list needed to calculate the changes
         profit_losslist.append(int(row[1]))
 
+        #store the content of colunm 0 in the date variable 
         date = row[0] 
+
+        #store the content of colunm 1 in the profit variable 
         profit = int(row[1])
     
-        
+        #check for the previous profit/loss
         if prev_month_profit is not None:
-            # Calculate the change from the previous month
+            # Calculate the change from the previous month if the previous month has a value
             change = profit - prev_month_profit
             
 
-    # Update the greatest change and date if this month's change is greater
+        #Update the greatest change and date if this month's change is greater
         if greatest_increase is None or change > greatest_increase:
+            #the change will become the new greatest increase if it is bigger than the previous greatest increase
             greatest_increase = change
+            #pick up the date corresponding to the new greatest increase
             greatest_increase_date = date
             
 
-    # Update the tracking variables if this is the greatest decrease
+        # Update the tracking variables if this is the greatest decrease
         if greatest_decrease is None or change < greatest_decrease:
+            #the change will become the new greatest decrease if it is smaller than the previous greatest decrease
             greatest_decrease = change
+            #pick up the date corresponding to the new greatest decrease
             greatest_decrease_date = date
 
             
-    #Update the previous month's profit for the next iteration
+        #Update the previous month's profit for the next iteration
         prev_month_profit = profit
 
-
+#Finding difference between the current profit/loss value and previous profitloss value using all values in list created
 profitloss_changes = [(profit_losslist[i+1] - profit_losslist[i]) for i in range(len(profit_losslist)-1)]
+
+#adding the changes then diving by number of months and rounding the answer to two decimal places
 average_changes = round(sum(profitloss_changes)/(row_count-1),2)
 
 print("Financial Analysis")
@@ -78,9 +96,10 @@ print(f"The greatest decrease in profits was in {greatest_decrease_date} with an
 # Specify the file to write to
 output_path = os.path.join("analysis", "pyBankoutputs.txt")
 
-# Open the file using "write" mode. Specify the variable to hold the contents
+# Open the file using "write" mode. Specify the file variable to hold the contents
 with open(output_path, 'w') as outfile:
 
+    #output the results onto the textfile
     outfile.write("Financial Analysis"+'\n')
     outfile.write("------------------------------"+'\n')
     outfile.write("Total months: "+ str(row_count)+'\n')
